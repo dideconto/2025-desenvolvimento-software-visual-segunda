9ca9c2a087ea77279bdd920b42ab4ef0567ba486
Diogo Steinke Deconto <diogo.deconto@pf.up.edu.br>
2025-08-18 21:48:10 -0300

    API- Projeto base

---
+++ Ecommerce/API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//Funcionalidade - Requisições
+// - URL/Caminho/Endereço
+// - Um método HTTP
+
+app.MapGet("/", () => "Minha segunda API em C#");
+
+app.MapGet("/endereco", () => "Outra funcionalidade");
+
+app.MapPost("/endereco", () => "Funcionalidade do POST");
+
+app.Run();
