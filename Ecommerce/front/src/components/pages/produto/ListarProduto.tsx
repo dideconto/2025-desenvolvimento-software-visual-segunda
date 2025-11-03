525a1821fde8e0a2551759fcde774131aa3a642f
Diogo Steinke Deconto <dideconto@gmail.com>
2025-11-03 20:41:04 -0300

    FRONT - Listar produtos

--- Ecommerce/front/src/components/pages/produto/ListarProduto.tsx
+++ Ecommerce/front/src/components/pages/produto/ListarProduto.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 //Regras para criação de um componente
 //1 - Componente deve ser uma função
@@ -7,6 +7,10 @@ import { useEffect } from "react";
 //seja utilizado na aplicação
 
 function ListarProdutos() {
+  //Estados | Variáveis
+  //const [nome, setNome] = useState("Diogo Steinke Deconto");
+  const [produtos, setProdutos] = useState<[]>([]);
+
   //useEffect é utilizado para executar algum código no
   //momento em que o componente é carregado no navegador
   useEffect(() => {
@@ -19,13 +23,11 @@ function ListarProdutos() {
       const resposta = await fetch(
         "http://localhost:5190/api/produto/listar"
       );
-
       if (!resposta.ok) {
         throw new Error("Erro na requisição: " + resposta.statusText);
       }
-
       const dados = await resposta.json();
-      console.log(dados);
+      setProdutos(dados);
     } catch (error) {
       console.log(error);
     }
@@ -34,6 +36,28 @@ function ListarProdutos() {
   return (
     <div id="componente_listar_produtos">
       <h1>Listar Produtos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Criado em</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map((produto: any) => (
+            <tr>
+              <td>{produto.id}</td>
+              <td>{produto.nome}</td>
+              <td>{produto.preco}</td>
+              <td>{produto.quantidade}</td>
+              <td>{produto.criadoEm}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
